1febfea9acc1fb0dd614b52e8ded56c68edc2bc0
tbrown034 <tbrown034@gmail.com>
2024-12-13 19:09:50 -0500

    restored Details, everything working as expected

--- app/movies/MoviesList/FullList.jsx
+++ app/movies/MoviesList/FullList.jsx
@@ -12,6 +12,7 @@ export default function FullList({ movies, userList, addToUserList }) {
             key={movie.id}
             className="flex items-center justify-between py-2 border-b cursor-pointer hover:bg-gray-100"
           >
+            {/* Movie Title as a Link */}
             <Link
               href={`/movies/${movie.id}`}
               className="flex-1 text-blue-600 hover:underline"
@@ -19,6 +20,15 @@ export default function FullList({ movies, userList, addToUserList }) {
               {movie.title}
             </Link>
             <div className="flex gap-2">
+              {/* Details Button as a Link */}
+              <Link
+                href={`/movies/${movie.id}`}
+                className="px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-800"
+              >
+                Details
+              </Link>
+
+              {/* Add to List Button */}
               <AddToListButton
                 movie={movie}
                 onAdd={() => addToUserList(movie)}
