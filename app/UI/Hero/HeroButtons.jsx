68e23f5846a002a5e42748b97eb2eb86cf9db50f
tbrown034 <tbrown034@gmail.com>
2024-12-18 21:12:22 -0500

    added auth, works with github in dev mode

--- app/UI/Hero/HeroButtons.jsx
+++ app/UI/Hero/HeroButtons.jsx
@@ -1,22 +1,9 @@
-import Link from "next/link";
+import SignIn from "../components/sign-in";
 
 const HeroButtons = () => {
-  const primaryLinks = [
-    { href: "/movies", label: "Get Started" },
-    { href: "/login", label: "Log In" },
-  ];
-
   return (
     <div className="flex gap-4">
-      {primaryLinks.map((link, index) => (
-        <Link
-          key={index}
-          href={link.href}
-          className="px-4 py-2 text-xl font-bold text-black transition border-2 border-black rounded-xl hover:bg-black hover:text-white"
-        >
-          {link.label}
-        </Link>
-      ))}
+      <SignIn />
     </div>
   );
 };
---
+++ auth.js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GitHub({
+      clientId: isDevelopment
+        ? process.env.AUTH_GITHUB_ID_DEV
+        : process.env.AUTH_GITHUB_ID,
+      clientSecret: isDevelopment
+        ? process.env.AUTH_GITHUB_SECRET_DEV
+        : process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
+});
