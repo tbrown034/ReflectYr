c689af8cb8a53e3e13562bd8f98183c2a2e80961
tbrown034 <tbrown034@gmail.com>
2025-01-18 16:38:12 -0500

    added text file and using next/font to add new fonts to heroText

--- app/UI/Hero/HeroText.jsx
+++ app/UI/Hero/HeroText.jsx
@@ -1,17 +1,23 @@
+import { roboto_mono, inter, rubik } from "../../fonts/fonts.js";
+
 const HeroText = () => {
   return (
-    <div className="flex flex-col items-center gap-10 text-center md:items-start md:text-left">
+    <div className="flex flex-col items-center gap-6 text-center md:items-start md:text-left">
       {/* Main Heading */}
-      <h1 className="text-5xl font-extrabold leading-tight md:text-6xl">
+      <h1
+        className={`text-5xl font-extrabold leading-tight md:text-6xl ${rubik.className} `}
+      >
         <span className="text-amber-600 dark:text-amber-400 ">ReflectYr:</span>{" "}
         <br className="hidden md:block" />
-        <span className="text-gray-800 dark:text-gray-200">
+        <span className={`text-gray-800 dark:text-gray-200 ${inter.className}`}>
           Your Year in Film In Review
         </span>
       </h1>
 
       {/* Subheading */}
-      <p className="mx-auto text-lg leading-relaxed text-gray-800 dark:text-gray-200 md:text-xl md:mx-0">
+      <p
+        className={`mx-auto text-lg  text-gray-800 dark:text-gray-200 md:text-xl md:mx-0 ${roboto_mono.className}`}
+      >
         Celebrate your year. Curate your favorite movies from the past 12
         months. Build your list and share it with friends—or just reflect for
         yourself.
---
+++ app/fonts/fonts.js
@@ -0,0 +1,16 @@
+import { Inter, Roboto_Mono, Rubik } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const rubik = Rubik({
+  subsets: ["latin"],
+  display: "swap",
+});
