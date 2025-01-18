034a7e9707f5f4ca988511340855bb9cf33fe005
tbrown034 <tbrown034@gmail.com>
2025-01-18 18:03:30 -0500

    new fonts

--- app/UI/Hero/HeroText.jsx
+++ app/UI/Hero/HeroText.jsx
@@ -1,22 +1,26 @@
-import { roboto_mono, inter, rubik } from "../../fonts/fonts.js";
+import { rubik, inter, raleway } from "../../fonts/fonts.js";
 
 const HeroText = () => {
   return (
     <div className="flex flex-col items-center gap-6 text-center md:items-start md:text-left">
       {/* Main Heading */}
       <h1
-        className={`text-5xl font-extrabold leading-tight md:text-6xl ${rubik.className} `}
+        className={`text-5xl font-extrabold leading-tight md:text-6xl ${raleway.className}`}
       >
-        <span className="text-amber-600 dark:text-amber-400 ">ReflectYr:</span>{" "}
+        <span
+          className={`text-amber-600 dark:text-amber-400 ${rubik.className}`}
+        >
+          ReflectYr:
+        </span>{" "}
         <br className="hidden md:block" />
-        <span className={`text-gray-800 dark:text-gray-200 ${inter.className}`}>
+        <span className="text-gray-800 dark:text-gray-200">
           Your Year in Film In Review
         </span>
       </h1>
 
       {/* Subheading */}
       <p
-        className={`mx-auto text-lg  text-gray-800 dark:text-gray-200 md:text-xl md:mx-0 ${roboto_mono.className}`}
+        className={`mx-auto text-lg text-gray-800 dark:text-gray-200 md:text-xl md:mx-0 ${inter.className}`}
       >
         Celebrate your year. Curate your favorite movies from the past 12
         months. Build your list and share it with friends—or just reflect for
--- app/fonts/fonts.js
+++ app/fonts/fonts.js
@@ -1,4 +1,13 @@
-import { Inter, Roboto_Mono, Rubik } from "next/font/google";
+import {
+  Inter,
+  Roboto_Mono,
+  Rubik,
+  Merriweather,
+  Nunito,
+  Raleway,
+  Playfair_Display,
+  Montserrat,
+} from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -12,5 +21,35 @@ export const roboto_mono = Roboto_Mono({
 
 export const rubik = Rubik({
   subsets: ["latin"],
+  display: "swap",
+});
+
+export const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+});
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
+  display: "swap",
+});
+
+export const playfair_display = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"],
   display: "swap",
 });
