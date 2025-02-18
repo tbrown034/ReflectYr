03ce94b6b758a178fef0bb1d22699048b3b11306
tbrown034 <tbrown034@gmail.com>
2025-02-18 17:35:37 -0500

    fix listwrapper file err

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -1,5 +1,5 @@
 import { fetchDiscoverMovies, fetchSearchResults } from "@/app/api/movies";
-import ListWrapper from "./ListsWrapper";
+import ListWrapper from "../ListsWrapper";
 
 export default async function MoviesPage({
   searchParams: searchParamsPromise,
